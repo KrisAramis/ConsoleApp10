7a49c4bca296a4474b2bce4734969d16c3db604a
krisaramis <kristina.kulitsch@gmail.com>
2020-04-30 09:32:37 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st triangle side:");
+            int a= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 2nd triangle side:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 3rd triangle side:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int[] i =  { a, b, c };
+            int maxvalue = i.Max(); 
+
+            if (a == b && b== c && a==c)
+                Console.WriteLine("Triange is равносторонний");
+            else if (a == b || b == c || a == c)
+            { 
+                Console.WriteLine("Triange is равнобедренный");
+            }
+            else if ( maxvalue ==a && ) 
+        }
+    }
+}
